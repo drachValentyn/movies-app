fb344ebcbeffd870024a02c883e773a29eda428b
valik <valentynd91@gmail.com>
2020-12-08 21:12:35 +0200

    add api

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <MoviesList :list="moviesList" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import { mapActions, mapGetters } from "vuex";
+import MoviesList from "@/components/MoviesList";
 export default {
   name: "App",
   components: {
-    HelloWorld
+    MoviesList
+  },
+  computed: {
+    ...mapGetters("movies", ["moviesList"])
+  },
+  mounted() {
+    this.fetchMovies();
+  },
+  methods: {
+    ...mapActions("movies", ["fetchMovies"])
   }
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Arial, Helvetica, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
